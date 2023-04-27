e954367997ddd6bd08c3296a0f8dc0e6a0e44778
Jakub Lemiesiewicz <jlemiesiewicz@man.poznan.pl>
2023-04-27 13:00:43 +0200

    hello example

---
+++ server.py
@@ -0,0 +1,28 @@
+import grpc
+from concurrent import futures
+import time
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+class Greeter(helloworld_pb2_grpc.GreeterServicer):
+
+    def SayHello(self, request, context):
+        return helloworld_pb2.HelloReply(message=f"Hello, {request.name}!")
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server started on port 50051.")
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    serve()
